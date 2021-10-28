fbf6792b279a1ae5497547bb407e03393919dae5
glenert41 <glenert41@charlottesvilleschools.org>
2021-10-28 17:16:13 +0100

    basic button style

--- gui.py
+++ gui.py
@@ -8,13 +8,20 @@ frame = tk.Frame(root)
 def onclick(args):
     
     if args == "lightBtnOn":
-        print("Lights are on")
+        print("Lights are On")
     if args == "lightBtnOff":
         print("Lights are Off")
 
 
 #create button elements
-lightBtnOn = tk.Button(root, text="Lights On",command=lambda:onclick("lightBtnOn"))
+lightBtnOn = tk.Button(root, text="Lights On",
+                       command=lambda:onclick("lightBtnOn"),
+                       fg = "#37d3ff",
+                       bg = "#001d26",
+                       highlightthickness=4,
+                       highlightcolor="#37d3ff",
+                       highlightbackground="#37d3ff",
+                       borderwidth=4)
 lightBtnOff = tk.Button(root, text="Lights Off",command=lambda:onclick("lightBtnOff"))
 
 
