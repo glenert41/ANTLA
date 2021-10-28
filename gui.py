89c0bc670e8997d2c919a389ec36b768a925377c
glenert41 <glenert41@charlottesvilleschools.org>
2021-10-28 17:46:34 +0100

    flat button

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ import tkinter as tk
 root = tk.Tk()
 root.title("ANTLA")
 frame = tk.Frame(root)
+relief=tk.FLAT
 
 
 def onclick(args):
@@ -14,14 +15,15 @@ def onclick(args):
 
 
 #create button elements
-lightBtnOn = tk.Button(root, text="Lights On",
+lightBtnOn = tk.Button(root, text="Lights On", relief=tk.FLAT,
                        command=lambda:onclick("lightBtnOn"),
-                       fg = "#37d3ff",
-                       bg = "#001d26",
-                       highlightthickness=4,
+                       fg = "#ffffff",
+                       bg = "#37d3ff",
+                       activebackground="#3791ff",
+                       highlightthickness=0,
                        highlightcolor="#37d3ff",
                        highlightbackground="#37d3ff",
-                       borderwidth=4)
+                       borderwidth=2)
 lightBtnOff = tk.Button(root, text="Lights Off",command=lambda:onclick("lightBtnOff"))
 
 
@@ -29,4 +31,6 @@ lightBtnOff = tk.Button(root, text="Lights Off",command=lambda:onclick("lightBtn
 lightBtnOn.pack()
 lightBtnOff.pack()
 
+root.geometry("400x400")
+root.configure(bg="#FFFDD0")
 root.mainloop()
