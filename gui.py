cf2bb90635b8e6f9c6fd4c751387c267cca6f4f4
glenert41 <glenert41@charlottesvilleschools.org>
2021-11-10 17:30:42 +0000

    button/label update works; trying to style buttons now

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter.ttk import *
 #import keyboard
 
 root = tk.Tk()
@@ -19,18 +21,26 @@ queenBlue = "#4D7298"
 #ffffff is white
 white = "#ffffff"
 
+buttonStyle = Style()
+buttonStyle.configure("buttonStyle", foreground = "red")
+
+
+
+
+
 lightVar = tk.StringVar()
+lightVar.set("No Status Chosen; Lights off")
+
 
 def onclick(args):
     
     if args == "lightBtnOn":
         print("Lights are On")
-        #lightVar = str("Lights are On")
-        lightVar.set("Lights On")
+        lightVar.set("Status: Lights are On")
+       
     if args == "lightBtnOff":
         print("Lights are Off")
-        #lightVar = str("Lights are Off")
-        lightVar.set("Lights Off")
+        lightVar.set("Status: Lights are Off")
 
 
 #create button elements
@@ -43,15 +53,13 @@ lightBtnOn = tk.Button(root, text="Lights On", relief=tk.FLAT,
                        highlightcolor= blueLavender,
                        highlightbackground= blueLavender,
                        borderwidth=2)
-lightBtnOff = tk.Button(root, text="Lights Off", relief=tk.FLAT,
-                       command=lambda:onclick("lightBtnOff"),
-                       fg = white,
-                       bg = blueLavender, #button color 
-                       activebackground=queenBlue,
-                       highlightthickness=0,
-                       highlightcolor= blueLavender,
-                       highlightbackground= blueLavender,
-                       borderwidth=2)
+lightBtnOff = ttk.Button(root, text="Lights Off", style="buttonStyle",
+                       command=lambda:onclick("lightBtnOff")
+                       )
+lightLabel = tk.Label(root,textvariable=lightVar,
+                      fg=white,
+                      bg = blueLavender)
+
 '''
 if keyboard.on_press_key("w"):
     print("W is pressed")
@@ -62,17 +70,14 @@ if keyboard.on_press_key("s"):
 
 
 
-lightLabel = tk.Label(root,text=lightVar)
-
-
-
 #Put elements on main window
 lightBtnOn.pack()
 lightBtnOff.pack()
 lightLabel.pack()
 
 
+
 root.geometry("400x400")
 root.configure(bg=darkPurple)
 root.mainloop()
-root.update()
+
